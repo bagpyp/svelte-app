24e413a58237c546eb6ab298bbe1113d09950d2e
robertcunningham <robertcunningham@ford.com>
2023-07-17 12:30:21 -0700

    wrap h2 placeholder in AuthCheck component

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-    
+    import AuthCheck from "$lib/components/AuthCheck.svelte"
+    import { db, user } from "$lib/firebase"
+    import { doc, getDoc, writeBatch } from "firebase/firestore"
 </script>
 
-<h2>Username</h2>
+<AuthCheck>
+  <h2>Username</h2>
+</AuthCheck>
