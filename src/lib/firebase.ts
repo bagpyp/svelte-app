853e60da2947b238023c8da420960b3883c9f3cc
robertcunningham <robertcunningham@ford.com>
2023-07-18 11:49:57 -0700

    userData store subscription

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
 import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { writable } from "svelte/store";
+import { writable, type Readable, derived } from "svelte/store";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDwjWoSomDrJLzPr37N_8w-h3y5lfbMTxw",
@@ -21,7 +21,9 @@ export const auth = getAuth();
 export const storage = getStorage();
 
 
-
+/**
+ * @returns a store with the current firebase user
+ */
 function userStore() {
   let unsubscribe: () => void;
 
@@ -47,3 +49,48 @@ function userStore() {
 }
 
 export const user = userStore();
+
+
+
+/**
+ * @param  {string} path document path or reference
+ * @param  {any} startWith optional default data
+ * @returns a store with realtime updates on document data
+ */
+export function docStore<T>(
+  path: string,
+) {
+  let unsubscribe: () => void;
+
+  const docRef = doc(db, path);
+
+  const { subscribe } = writable<T | null>(null, (set) => {
+    unsubscribe = onSnapshot(docRef, (snapshot) => {
+      set((snapshot.data() as T) ?? null);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+    ref: docRef,
+    id: docRef.id,
+  };
+}
+
+interface UserData {
+  username: string;
+  bio: string;
+  photoURL: string;
+  published: boolean;
+  links: any[];
+}
+
+export const userData: Readable<UserData | null> = derived(user, ($user, set) => { 
+  if ($user) {
+    return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+  } else {
+    set(null); 
+  }
+});  
